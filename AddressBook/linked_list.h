e141305750974f8aee4ebeac0dcfe89cc3880e46
PauliusJascemskas <116302568+PauliusJascemskas@users.noreply.github.com>
2024-02-05 16:55:03 +0200

    Delete AddressBook directory

--- AddressBook/linked_list.h
+++
@@ -1,8 +0,0 @@
-struct Address* create_node(char *name, char *surname, char *email, char *phone);
-void get_all_nodes(struct Address **pnt);
-void add_node_to_list(struct Address **pnt, struct Address *address);
-void add_node_to_position(struct Address **pnt, struct Address *address, int index);
-void remove_one_node(struct Address **pnt, int index);
-void remove_all_nodes(struct Address **pnt);
-void find_node_by_position(struct Address **pnt, int index);
-void find_node_by_attribute(struct Address **pnt, char attribute[], char value[]);
